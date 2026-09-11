c8223b83921ce668b8d9ca3e0b30cf797a2fba6c
ubaid1294109 <ubaidrazashaikh88@gmail.com>
2026-09-11 20:06:51 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const studentRoutes = require("./routes/studentRoutes");
-const courseRoutes = require("./routes/courseRoutes");
-const contectRoutes = require("./routes/contectRoutes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/students", studentRoutes);
-app.use("/api/course", courseRoutes);
-app.use("/api/contect", contectRoutes);
-
-app.get("/", (req, res) => {
-  res.send("LearnHub API is running");
-});
-
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose
-  .connect(MONGO_URI)
-=======
-//  require("dotenv").config();
-//  const express = require("express");
-//  const mongoose =require("mongoose");
-//  const cors = require("cors");
-
-//  const studentRoutes = require("./routes/studentRoutes");
-//  const courseRoutes = require("./routes/courseRoutes");
-//  const contectRoutes = require("./routes/contectRoutes");
-
-//  const app = express();
-
-//  app.use(cors());
-//  app.use(express.json());
-
-//  app.use("/api/students", studentRoutes);
-//  app.use("/api/course", courseRoutes);
-//  app.use("/api/contect", contectRoutes);
-
-//  app.get("/",(req,res)=>{
-//     res.send("learnHub API is running");
-//  });
-//  const PORT = process.env.PORT || 5000;
-
-//  mongoose.connect(process.env.MONGO_URI)
-//  .then(()=>{console.log("MongoDB connected");
-
-//  app.listen(PORT, () =>console.log(`server running on http://localhost:${PORT}` ))
-//  })
-//  .catch((err)=>console.error("MongoDB.connection error:", err.message));
-
-// //ubaidrazashaikh88_db_user
-// // mongodb+srv://ubaidrazashaikh88_db_user:IxKro79l9bGkI4vu@cluster0.os8y7s4.mongodb.net/
-// //IxKro79l9bGkI4vu
-
-
 require("dotenv").config();
 
 const express = require("express");
@@ -87,7 +23,6 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(process.env.MONGO_URI)
->>>>>>> edfc91e (Fix Vercel API deployment)
   .then(() => {
     console.log("MongoDB connected");
   })
@@ -95,15 +30,9 @@ mongoose
     console.error("MongoDB connection error:", err.message);
   });
 
-<<<<<<< HEAD
-// Important for Vercel
 module.exports = app;
 
 // Local development
-=======
-module.exports = app;
-
->>>>>>> edfc91e (Fix Vercel API deployment)
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
 
